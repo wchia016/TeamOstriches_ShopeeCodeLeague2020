ed4c8f2b37160d3f95d586f2b91cf32d21685969
Chia Wei Fong <65015142+wchia016@users.noreply.github.com>
2020-06-16 09:01:34 +0800

    Add files via upload

--- Personal Tinkering/Week 1/Order_Brushing.py
+++ Personal Tinkering/Week 1/Order_Brushing.py
@@ -1,22 +1,21 @@
 import numpy as np
 import pandas as pd
-import multiprocessing as mp
 from datetime import datetime
 from collections import defaultdict
 import os
 
 #Read Data
 os.chdir('C:\\Users\\Wei Fong\\Desktop')
-df = pd.read_csv('C:\\Users\\Wei Fong\\Desktop\\order_brush_order.csv')
-df['event_time'] = pd.to_datetime(df['event_time'])
-df.info()
+data = pd.read_csv('C:\\Users\\Wei Fong\\Desktop\\order_brush_order.csv')
+data['event_time'] = pd.to_datetime(data['event_time'])
+data.info()
 
 #Filter out stores with 2 or less orders
-store_ser = df.groupby(['shopid']).size()
-shops_to_check = store_ser[store_ser > 3]
+store_series = data.groupby(['shopid']).size()
+shops_to_check = store_series[store_series > 3]
 
 #Sort remaining stores by chronological order
-df = df[df['shopid'].isin(shops_to_check.index)]
+df = data[data['shopid'].isin(shops_to_check.index)]
 df = df.sort_values(by='event_time')
 df.set_index('event_time', inplace = True)
 
@@ -32,14 +31,13 @@ order_brush_shops.set_index(['shopid', event_time], inplace=True)
 
 #Identify shop brushing userid
 multi_index = list(order_brush_shops.index)
-df = pd.read_csv('C:\\Users\\Wei Fong\\Desktop\\order_brush_order.csv')
-df = df[df['shopid'].isin(shops_to_check.index)]
+data = pd.read_csv('C:\\Users\\Wei Fong\\Desktop\\order_brush_order.csv')
+df = data[data['shopid'].isin(shops_to_check.index)]
 df = df.sort_values(by='event_time')
 df.set_index(['shopid','event_time'], inplace = True)
 
 sus_userid = df.loc[multi_index]
 sus_userid.reset_index(['shopid','event_time'], inplace=True)
-sus_userid['shopid'] = sus_userid['shopid'].astype(str)
 sus_userid['userid'] = sus_userid['userid'].astype(str)
 
 #Clean up data of suspicious shops
@@ -48,5 +46,20 @@ result = defaultdict(list)
 for i in range(len(sus_userid)):
     result[sus_userid['shopid'][i]].append(sus_userid['userid'][i])
 
+prediction = pd.DataFrame([store_series.index, np.zeros(len(store_series), dtype=int)]).transpose()
+prediction.columns = ['shopid','userid']
+prediction.set_index('shopid', inplace=True)
+prediction['userid'] = prediction['userid'].astype(str)
 
-
+for i in sus_userid.groupby(['shopid']).size().index:
+    temp = result[i]
+    for j in temp:
+        if len(temp) == 1:
+            prediction['userid'][i] = j
+        else:
+            prediction['userid'][i] = '&'.join(temp)
+            
+compression_opts = dict(method='zip',
+                        archive_name='prediction.csv')  
+prediction.to_csv('out.zip', index=False,
+          compression=compression_opts) 
